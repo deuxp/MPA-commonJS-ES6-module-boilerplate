bf74c3232df7e4bb1d0e206b88555c4426e3aadc
Gottfried Kleinberger <friedi.frieds@gmail.com>
2023-02-19 00:48:55 -0500

    init

---
+++ src/helper.js
@@ -0,0 +1,5 @@
+export const printLog = word => {
+  console.log("module!" + word);
+};
+
+export const timesFive = n => 5 * n;
---
+++ src/main.js
@@ -0,0 +1,2 @@
+import { printLog, timesFive } from "./helper.js";
+console.log("5 x 5 = ", timesFive(5));
