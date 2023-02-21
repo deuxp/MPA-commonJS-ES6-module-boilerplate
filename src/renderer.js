b224d39481fd08ba77f96e87dd530af304333b43
Gottfried Kleinberger <friedi.frieds@gmail.com>
2023-02-21 14:04:12 -0500

    wip

--- src/renderer.js
+++ src/renderer.js
@@ -1,17 +1,13 @@
 // imports must have .js extention included in path
 import { links } from "./db/data.js";
-import {
-  createNewNode,
-  createUnorderedList,
-  addAttributes,
-} from "./helpers.js";
+import { createNewNode, createUnorderedList } from "./helpers.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const root = document.querySelector("#root");
+  const ROOT = document.querySelector("#root");
 
   const header = createNewNode("h1", "header", "Welcome");
-  root.appendChild(header);
+  ROOT.appendChild(header);
 
   const ul = createUnorderedList(links, "list");
-  root.appendChild(ul);
+  ROOT.appendChild(ul);
 });
