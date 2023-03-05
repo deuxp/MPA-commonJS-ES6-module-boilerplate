3454d369f88749ef9dea9436cf6ab6b61b328eb4
Gottfried Kleinberger <friedi.frieds@gmail.com>
2023-03-05 11:23:01 -0500

    clean out electron

--- src/scripts/renderer.js
+++ src/scripts/renderer.js
@@ -1,6 +1,6 @@
 // imports must have .js extention included in path
-import { links } from "./db/data.js";
-import { createNewNode, createUnorderedList } from "./helpers.js";
+import { links } from "../db/data.js";
+import { createNewNode, createUnorderedList } from "../helpers/helpers.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const ROOT = document.querySelector("#root");
